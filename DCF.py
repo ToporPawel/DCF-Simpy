53f8b25e067bcb247b80b4e570fb16466d194ffc
ToporPawel <paweltopor13@gmail.com>
2020-05-15 22:27:06 +0200

    Add first version of DCF-meeting 2

---
+++ DCF.py
@@ -0,0 +1,126 @@
+import random
+import simpy
+
+CW_MIN = 15
+ALL_STATIONS = None
+TRANSMITTED_FRAMES = []
+SIMULATION_TIME = 500
+NUM_OF_STATIONS = 5
+FAILED_TRANSMISSIONS = 0
+SUCCEEDED_TRANSMISSIONS = 0
+FRAME_MIN_TIME = 5
+FRAME_MAX_TIME = 20
+
+# TODO use logger instead of print
+# TODO Clean up and split into more transparent functions
+# TODO Validate if working properly
+# TODO use different colors per station output
+
+
+class Frame:
+
+    def __init__(self, frame_length, station_name):
+        self.frame_length = frame_length
+        self.number_of_retransmissions = 0
+        self.t_start = environment.now
+        self.t_end = None
+        self.t_to_send = None
+        self.station_name = station_name
+
+    def __repr__(self):
+        return "Frame: start=%d, end=%d, frame_time=%d, retransmissions=%d" \
+               % (self.t_start, self.t_end, self.t_to_send, self.number_of_retransmissions)
+
+
+def generate_new_frame(station_name):
+    frame_length = random.randrange(FRAME_MIN_TIME, FRAME_MAX_TIME)
+    return Frame(frame_length, station_name)
+
+
+def generate_new_backoff():
+    return random.randrange(1, CW_MIN)
+
+
+class Station(object):
+
+    def __init__(self, env, transmission_resource, transmission_store, name):
+        self.name = name
+        self.transmission_resource = transmission_resource
+        self.transmission_store = transmission_store
+        self.env = env
+        self.frame_to_send = None
+        self.process = env.process(self.wait_to_send())
+
+    def wait_to_send(self):
+        while True:
+            was_sent_completed = False
+            self.frame_to_send = generate_new_frame(self.name)
+            print("New frame generated from station:{} , time: {}".format(self.name, self.env.now))
+            while not was_sent_completed:
+                back_off = generate_new_backoff()
+                print("Initial backoff: {} for station: {}".format(back_off, self.name))
+                while back_off:
+                    print("In while backoff {}of station: {}".format(back_off, self.name))
+                    start = self.env.now
+                    try:
+                        print("Waiting backoff to send frame from station: {} time: {}".format(self.name, self.env.now))
+                        yield self.env.timeout(back_off)
+                        back_off = 0
+                    except simpy.Interrupt:
+                        print("Waiting was interrupted, waiting to resume backoff from station: {}, time:{}"
+                              .format(self.name, self.env.now))
+                        back_off -= self.env.now - start
+                        try:
+                            with self.transmission_resource.request() as req:
+                                yield req
+                        except simpy.Interrupt:
+                            print("Frame holding lock was sent, resuming backoff from station: {}, time: {}"
+                                  .format(self.name, self.env.now))
+                try:
+                    print("Backoff waited, sending frame from station: {}, time: {}".format(self.name, self.env.now))
+                    yield self.transmission_store.put(self)
+                    with self.transmission_resource.request(self.frame_to_send.frame_length) as req:
+                        for station in ALL_STATIONS:
+                            if station not in self.transmission_store.items:
+                                print("Informing other station {} about frame transmission started from station: "
+                                      "{}, time: {}".format(station.name, self.name, self.env.now))
+                                station.process.interrupt()
+                        print("Holding transmission lock from station:{}, time:{}".format(self.name, self.env.now))
+                        yield req
+                        yield self.env.timeout(self.frame_to_send.frame_length)
+                        print("Check if there was any collision..., time: {}".format(self.env.now))
+                        if len(self.transmission_store.items) != 1:
+                            print("There was collision, retrying, affected frames:{} time:{}"
+                                  .format(len(self.transmission_store.items), self.env.now))
+                            for i in range(len(self.transmission_store.items)):
+                                station = yield self.transmission_store.get()
+                                if station != self:
+                                    station.process.interrupt()
+                                    station.frame_to_send.number_of_retransmissions += 1
+                            self.frame_to_send.number_of_retransmissions += 1
+                            raise simpy.Interrupt(None)
+                        print("Successfully sent frame from station:{}, time: {}".format(self.name, self.env.now))
+                        was_sent_completed = True
+                        self.frame_to_send.t_end = self.env.now
+                        self.frame_to_send.t_to_send = self.frame_to_send.t_end - self.frame_to_send.t_start
+                        TRANSMITTED_FRAMES.append(self.frame_to_send)
+                        global SUCCEEDED_TRANSMISSIONS
+                        SUCCEEDED_TRANSMISSIONS += 1
+                except simpy.Interrupt:
+                    print("Collision from station: {}, time: {}".format(self.name, self.env.now))
+                    global FAILED_TRANSMISSIONS
+                    FAILED_TRANSMISSIONS += 1
+
+
+if __name__ == "__main__":
+    # random.seed(444)
+    environment = simpy.Environment()
+    transmission_priority_resource = simpy.PriorityResource(environment, capacity=1)
+    transmission_state_store = simpy.Store(environment)
+    ALL_STATIONS = [Station(environment, transmission_priority_resource, transmission_state_store, "Station{}"
+                            .format(i)) for i in range(NUM_OF_STATIONS)]
+    print(ALL_STATIONS)
+    environment.run(until=SIMULATION_TIME)
+    for frame in TRANSMITTED_FRAMES:
+        print(frame)
+    print("FAILED_TRANSMISSIONS: {}, SUCCEEDED_TRANSMISSION{}".format(FAILED_TRANSMISSIONS, SUCCEEDED_TRANSMISSIONS))
--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # PracaInzynierska
+
+
+A Python-based Simulator of Channel Access in IEEE 802.11 Networks using Simpy library
+
+
+## Installation
+
+- (Optional) Launch virtual env: `python3 -m venv env && source env/bin/activate`
+- Install requirements : `pip install -r requirements.txt`
+
+## Usage
+
+- `python3 DCF.py`
