6cb481743f3907fba6fe453bf16116b2996afdc2
ToporPawel <paweltopor13@gmail.com>
2020-05-28 11:45:41 +0200

    Add backoff

--- DCF.py
+++ DCF.py
@@ -4,18 +4,20 @@ import Colors
 import logging
 
 FRAME_LENGTH = 10
-CW_MIN = 1023
-ALL_STATIONS = None
-TRANSMITTED_FRAMES = []
-SIMULATION_TIME = 1000
-NUM_OF_STATIONS = 5
+CW_MIN = 15
+CW_MAX = 1023
+SIMULATION_TIME = 1000000
+NUM_OF_STATIONS = 10
+
+
 FAILED_TRANSMISSIONS = 0
 SUCCEEDED_TRANSMISSIONS = 0
-FRAME_MIN_TIME = 5
-FRAME_MAX_TIME = 20
-RES = Colors.get_normal()
+# FRAME_MIN_TIME = 5
+# FRAME_MAX_TIME = 20
+ALL_STATIONS = None
+TRANSMITTED_FRAMES = []
 TRANSMITTING_STATIONS = []
-logging.basicConfig(format='%(message)s', level=logging.INFO)
+logging.basicConfig(format='%(message)s', level=logging.ERROR)
 
 # TODO Clean up and split into more transparent functions
 
@@ -42,20 +44,21 @@ def generate_new_frame(station_name, output_color):
     return Frame(frame_length, station_name, output_color)
 
 
-def generate_new_backoff():
-    return random.randint(0, CW_MIN)
+def generate_new_backoff(n):
+    upper_limit = pow(2, n)*(CW_MIN+1)-1
+    upper_limit = upper_limit if upper_limit <= CW_MAX else CW_MAX
+    return random.randint(0, upper_limit)
 
 
 def log(station, mes):
-    logging.info(station.col + f"Time: {station.env.now} Station: {station.name} Message: {mes}" + RES)
+    logging.info(station.col + f"Time: {station.env.now} Station: {station.name} Message: {mes}" + Colors.get_normal())
 
 
 class Station(object):
 
-    def __init__(self, env, transmission_resource, transmission_store, name, output_color):
+    def __init__(self, env, transmission_resource, name, output_color):
         self.name = name
         self.transmission_resource = transmission_resource
-        # self.transmission_store = transmission_store
         self.env = env
         self.frame_to_send = None
         self.process = env.process(self.wait_to_send())
@@ -69,7 +72,7 @@ class Station(object):
             self.frame_to_send = generate_new_frame(self.name, self.col)
             log(self, "New frame generated")
             while not was_sent_completed:
-                back_off = generate_new_backoff()
+                back_off = generate_new_backoff(self.frame_to_send.number_of_retransmissions)
                 log(self, f"Initial backoff: {back_off}")
                 while back_off:
                     log(self, f"In while backoff {back_off}")
@@ -90,13 +93,7 @@ class Station(object):
                     log(self, "Backoff waited, sending frame")
                     TRANSMITTING_STATIONS.append(self)
                     with self.transmission_resource.request(self.frame_to_send.frame_length) as req:
-                        for station in ALL_STATIONS:
-                            if station not in TRANSMITTING_STATIONS:
-                                # print(self.col + "Informing station {} about frame transmission started from "
-                                #                  "station: {}, time: {}"
-                                #       .format(station.name, self.name, self.env.now) + RES)
-                                log(self, f"Informing station {station.name} about frame transmission started")
-                                station.process.interrupt()
+                        self.interrupt_other_station_backoff()
                         log(self, "Holding transmission lock")
                         yield req
                         yield self.env.timeout(self.frame_to_send.frame_length)
@@ -124,21 +121,27 @@ class Station(object):
                     log(self, "Collision")
                     global FAILED_TRANSMISSIONS
                     FAILED_TRANSMISSIONS += 1
-                    self.failed_transmissions +=1
+                    self.failed_transmissions += 1
+
+    def interrupt_other_station_backoff(self):
+        for station in ALL_STATIONS:
+            if station not in TRANSMITTING_STATIONS:
+                log(self, f"Informing station {station.name} about frame transmission started")
+                station.process.interrupt()
 
     def get_station_statistics(self):
         sum_failed_succeeded = 1 if self.succeeded_transmissions + self.failed_transmissions == 0 else self.succeeded_transmissions + self.failed_transmissions
         return self.col + f"Station name: {self.name}, Succeeded transmissions: {self.succeeded_transmissions}, Failed transmissions: " \
                f"{self.failed_transmissions}," \
-               f" PCOLL: {self.failed_transmissions/sum_failed_succeeded}" + RES
+               f" PCOLL: {self.failed_transmissions/sum_failed_succeeded}" + Colors.get_normal()
 
 
 if __name__ == "__main__":
-    # random.seed(444)
+    random.seed(444)
     environment = simpy.Environment()
     transmission_priority_resource = simpy.PriorityResource(environment, capacity=1)
     transmission_state_store = simpy.Store(environment)
-    ALL_STATIONS = [Station(environment, transmission_priority_resource, transmission_state_store, "Station{}"
+    ALL_STATIONS = [Station(environment, transmission_priority_resource, "Station{}"
                             .format(i), Colors.get_color()) for i in range(NUM_OF_STATIONS)]
     print(ALL_STATIONS)
     environment.run(until=SIMULATION_TIME)
